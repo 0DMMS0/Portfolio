4720150cbb7b0da53dad125f8a6b22218df40380
0DMMS0 <d.mau@outlook.es>
2025-08-09 20:40:21 -0600

    app.jsx and index.css added first ideas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
@@ -6,12 +6,16 @@ import Home from './components/Home';
 
 function App() {
   return (
-    <div>
+    <Router>
+      <div className="container">
       <Header />
-      <h1>Mi Portfolio</h1>
-      <p>Welcome to my WebSite 🚀</p>
-      <About />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      </div>
+    </Router>
   );
 }
 
